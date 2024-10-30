3e702b2735aa861ca77857c6268660efde6f6ba2
Franceskynov <franceskynov@gmail.com>
2024-10-29 22:17:38 -0600

    Simplify Format user data

--- actions/cli-actions.go
+++ actions/cli-actions.go
@@ -22,14 +22,12 @@ func GetRawData(userName string) (io.ReadCloser, error) {
 
 	switch response.StatusCode {
 	case http.StatusOK:
-		break
+		return response.Body, err
 	case http.StatusNotFound:
 		return nil, errors.New("are you typing the correct user name?")
 	default:
 		return nil, fmt.Errorf("errors on the server with status: %d", response.StatusCode)
 	}
-
-	return response.Body, err
 }
 
 /*
@@ -55,61 +53,38 @@ func GetUserData(userName string) []utils.UserEvent {
 	return userEvents
 }
 
+func ShowUserEvents(userName string, event utils.UserEvent) {
+	
+	switch event.Type {
+		case "ReleaseEvent":	
+			fmt.Printf("> Tagged a version %s of: %s published at: %s name: %s\n", event.Payload.Release.TagName, event.Repo.Name, event.Payload.Release.PublishedAt, event.Payload.Release.Name)
+		case "PushEvent":
+			fmt.Printf("> Pushed %d commit(s) to: %s at: %s \n", len(event.Payload.Commits), event.Repo.Name, event.CreatedAt)
+		case "WatchEvent":
+			if event.Payload.Action == "started" {
+				fmt.Printf("> User ⭐ to repo %s \n", event.Repo.Name)
+			}
+		case "IssueCommentEvent":
+			fmt.Println("-------------------------------------")
+			fmt.Printf("> Opened a new issue in: %s \nby: %s \ntitle: %s \nstatus: %s \n", event.Repo.Name, event.Payload.Issue.User.Login, event.Payload.Issue.Title, event.Payload.Issue.State)
+			fmt.Println("-------------------------------------")
+		case "PullRequestEvent":
+			if event.Payload.Action == "opened" {
+				fmt.Printf("> Opened a pull request in: %s created at: %s \n", event.Repo.Name, event.Payload.PullRequest.CreatedAt)
+			}
+			
+			if event.Payload.Action == "closed" {
+				fmt.Printf("> Closed a pull request in: %s closed at: %s \n", event.Repo.Name, event.Payload.PullRequest.ClosedAt)
+			}
+	}
+}
+
 func FormatUserData(userName string) {
 
 	userEvents := GetUserData(userName)
 	if len(userEvents) > 0 {
 		for _, event := range userEvents {
-
-			pushEvents := 0
-			issueCommentEvent := 0
-			pullRequestEventOpen := 0
-			pullRequestEventClosed := 0
-
-			switch event.Type {
-				// case "ReleaseEvent":	
-
-				case "PushEvent":
-					pushEvents = pushEvents + 1
-
-				case "WatchEvent":
-					if event.Payload.Action == "started" {
-					
-						fmt.Printf("> ⭐ to repo %s \n", event.Repo.Name)
-					}
-				case "IssueCommentEvent":
-					fmt.Printf("> Opened a new issue in: %s \nby: %s \ntitle: %s \nstatus: %s \n", event.Repo.Name, event.Payload.Issue.User.Login, event.Payload.Issue.Title, event.Payload.Issue.State)
-					fmt.Println("----------------------------------")
-					issueCommentEvent = issueCommentEvent + 1
-				case "PullRequestEvent":
-					if event.Payload.Action == "opened" {
-						pullRequestEventOpen = pullRequestEventOpen + 1
-						fmt.Printf("> User has opened a pull request in: %s \ncreated at: %s \n", event.Repo.Name, event.Payload.PullRequest.CreatedAt)
-						fmt.Println("----------------------------------")
-					}
-					
-					if event.Payload.Action == "closed" {
-						pullRequestEventClosed = pullRequestEventClosed + 1
-						fmt.Printf("> User has closed a pull request in: %s \nclosed at: %s \n", event.Repo.Name, event.Payload.PullRequest.ClosedAt)
-						fmt.Println("----------------------------------")
-					}
-			}
-
-			// if pushEvents > 0 {
-			// 	fmt.Printf("User push events %d to repo %s \n", pushEvents, event.Repo.Name)
-			// }
-
-	
-
-			// if pullRequestEventOpen > 0 {
-			// 	fmt.Printf("Number of pull request opened: %d \n", pullRequestEventOpen)
-			// }
-
-			// if pullRequestEventClosed > 0 {
-			// 	fmt.Printf("Number of pull request closed: %d \n", pullRequestEventClosed)
-			// }
+			ShowUserEvents(userName, event)
 		}
 	}
-
-
 }
